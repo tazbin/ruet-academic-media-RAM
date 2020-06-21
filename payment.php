0a9e2805cd17ddf664ba5bcd7d70eded5d0e98e5
tazbin <md.tazbinur.rahaman@gmail.com>
2020-06-21 20:45:10 +0600

    old files cleaned

--- payment.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $file = 'files/payment.png';
-  header('Content-type: application/pdf');
-  header('Content-Disposition: inline; filename="'.$file."");
-  header('Content-Trasfer-Encoding: binary');
-  header('Accept-Ranges: bytes');
-  @readfile($file);
- ?>
